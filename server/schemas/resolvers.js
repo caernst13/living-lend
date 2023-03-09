b032622452bfe1487237f9ac4f68856de7d21bf1
Steven-McCombe <smccombe93@gmail.com>
2023-03-08 20:00:48 -0500

    server testing - removed outdated code in the mongoose connection config. Added sample typedef and resolver to avoid error

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+
+//this is a basic example in order to test the server. Remove when adding actual code.
+const resolvers = {
+    Query: {
+      hello: () => 'Hello, world!'
+    }
+  };
+  
+  module.exports = resolvers;
