bfb86b9f3483c5554b3d8557e942de2f44740ddc
Steven-McCombe <smccombe93@gmail.com>
2023-03-09 22:53:35 -0500

    added routing and basic landing page.

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,12 +1,30 @@
+import { useState } from 'react'
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBIcon,
+  MDBCollapse,
+  MDBBadge
+} from 'mdb-react-ui-kit';
+// this is to prevent errors. We will need to add logic to the Nav for login
+//true represents a logged in user
+// false is logged out.
+const auth = true
+
 export default function Nav() {
     const [showNavSecond, setShowNavSecond] = useState(false);
   // function 
     function showNavigation() {
-      if (Auth.loggedIn()) {
+      if (auth) {
         return (
           <MDBNavbar expand='lg' light bgColor='light'>
             <MDBContainer fluid>
-              <MDBNavbarBrand href='#home'>Living Lend</MDBNavbarBrand>
+              <MDBNavbarBrand href='/'>Living Lend</MDBNavbarBrand>
               <MDBNavbarToggler
                 aria-expanded='false'
                 aria-label='Toggle navigation'
@@ -16,17 +34,16 @@ export default function Nav() {
               </MDBNavbarToggler>
               <MDBCollapse navbar show={showNavSecond}>
                 <MDBNavbarNav>
-                  <MDBNavbarLink active aria-current='page' href='#'>
+                  <MDBNavbarLink active aria-current='page' href='/'>
                     Home
                   </MDBNavbarLink>
-                  <MDBNavbarLink href='#home'>Home</MDBNavbarLink>
-                  <MDBNavbarLink href='#products'>Products</MDBNavbarLink>
-                  <MDBNavbarLink href='#orderhistory'>Order History</MDBNavbarLink>
-                  <MDBNavbarLink href='#logout'>Logout</MDBNavbarLink>
-                  <MDBNavbarItem>
+                  <MDBNavbarLink href='/products'>Products</MDBNavbarLink>
+                  <MDBNavbarLink href='/orderhistory'>Order History</MDBNavbarLink>
+                  <MDBNavbarLink href='/logout'>Logout</MDBNavbarLink>
+                  <MDBNavbarItem >
                     <MDBNavbarLink href='#cart'>
-                      <MDBBadge pill color='danger'>!</MDBBadge>
-                      <span>
+                      <MDBBadge pill color='danger'>0</MDBBadge>
+                      <span >
                         <MDBIcon fas icon='shopping-cart'></MDBIcon>
                       </span>
                     </MDBNavbarLink>
@@ -40,7 +57,7 @@ export default function Nav() {
         return (
           <MDBNavbar expand='lg' light bgColor='light'>
             <MDBContainer fluid>
-              <MDBNavbarBrand href='#home'>Living Lend</MDBNavbarBrand>
+              <MDBNavbarBrand href='/home'>Living Lend</MDBNavbarBrand>
               <MDBNavbarToggler
                 aria-expanded='false'
                 aria-label='Toggle navigation'
@@ -50,12 +67,9 @@ export default function Nav() {
               </MDBNavbarToggler>
               <MDBCollapse navbar show={showNavSecond}>
                 <MDBNavbarNav>
-                  <MDBNavbarLink active aria-current='page' href='#'>
-                    Home
-                  </MDBNavbarLink>
-                  <MDBNavbarLink href='#home'>Home</MDBNavbarLink>
-                  <MDBNavbarLink href='#products'>Products</MDBNavbarLink>
-                  <MDBNavbarLink href='#login'>Login/Signup</MDBNavbarLink>
+                  <MDBNavbarLink href='/'>Home</MDBNavbarLink>
+                  <MDBNavbarLink href='/products'>Products</MDBNavbarLink>
+                  <MDBNavbarLink href='/login'>Login/Signup</MDBNavbarLink>
                 </MDBNavbarNav>
               </MDBCollapse>
             </MDBContainer>
