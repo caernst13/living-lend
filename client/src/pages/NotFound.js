fcc8fe9a04de8b790b0f0cead04955efb5454568
Steven-McCombe <smccombe93@gmail.com>
2023-03-09 23:17:23 -0500

    added custom 404 page.

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,9 +1,23 @@
 import react from 'react'
-
+import { MDBBtn } from 'mdb-react-ui-kit'
 export default function NotFound() {
     return (
-    <div>
-        <h1>Page Not Found</h1>
+        <div className="container d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh" }}>
+            <span></span>
+            <h1 className="mb-2"><i class="fa-solid fa-person-circle-question"></i> 404</h1>
+            <h2 className="mb-2">UH OH! You're Lost</h2>
+            <p className="mb-2" style={{width: "300px"}}>The page you are looking for does not exist.
+          How you got here is a mystery. But you can click the button below
+                to go back to the products page.</p>
+                <MDBBtn
+                    color = 'dark'
+                    tag="a"
+                    size="m"
+                    rel="nofollow"
+                    href='/products'
+                  >
+                    Products
+                  </MDBBtn>
     </div>
     )
 
