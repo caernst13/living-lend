045e27cd28920c9aea0d721477d02a4e72af9f52
Steven-McCombe <smccombe93@gmail.com>
2023-03-10 22:37:01 -0500

    added state to login form, added login logic. Users can now sign up and log in. Working on adding auth logic to the nav bar

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,13 +22,24 @@ const resolvers = {
 
       return { token, user };
     },
+    //Mutation to login. Waits to see if there is a user with the input credentials.
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
-    },
-  }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  },
 }
   module.exports = resolvers;
