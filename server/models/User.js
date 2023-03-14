fd5f93dabb603aa1d294203258480514ee4be82f
Steven-McCombe <smccombe93@gmail.com>
2023-03-13 20:57:54 -0400

    added basic admin logic. Will need to add front end functionality. Updated Usermodel and seeds to allow for an array of orders

--- server/models/User.js
+++ server/models/User.js
@@ -25,12 +25,12 @@ const userSchema = new Schema({
     required: true,
     minlength: 8
   },
-  orders: [
-    {
-        type: Schema.Types.ObjectId,
-        ref: 'Order'
-      }
-  ]
+  orders: [Order.schema],
+
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
 });
 
 userSchema.pre('save', async function(next) {
